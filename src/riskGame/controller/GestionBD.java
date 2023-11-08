ac16c5f6505a8f68d71d39e6e70b30d1d9525f5b
TianYiyang99 <th1243768031@gmail.com>
2023-11-08 19:17:48 +0800

    Mettre SQL dans la class gestionBD- version2

--- src/riskGame/controller/GestionBD.java
+++ src/riskGame/controller/GestionBD.java
@@ -12,7 +12,6 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-import riskGame.PlayerRegistrationForm;
 import riskGame.model.EtatJoueur;
 import riskGame.model.EtatManche;
 import riskGame.model.Joueur;
@@ -48,33 +47,25 @@ public class GestionBD {
         }
     }
 
+
     public void creationJoueur(String nom,String prenom,String birthday,String equipe){
         try {
             Statement stmt = connection.createStatement();
             String query1 = "INSERT INTO equipe (nomEquipe, etatEquipe) VALUES('" + equipe + "', 'Valide')"; //用单引号把值括起来让数据库把他当作值而不是一列
             stmt.executeUpdate(query1);
-
             String query2 = "SELECT e.numeroEquipe FROM equipe e WHERE e.nomEquipe = '" + equipe + "'"; //在SQL查询中，字符串是单引号括起来的 所以这里相当于WHERE e.nomEquipe = 'equipe'
             ResultSet resultat = stmt.executeQuery(query2);
-
             int numeroEquipe = 0;
             if (resultat.next()) {
                 numeroEquipe = resultat.getInt("numeroEquipe");
             }
-
             String query3 = "INSERT INTO joueur (etatJoueur, nomJoueur, prenomJoueur, dateNaissanceJoueur, numeroEquipe) VALUES " +
                     "('VALIDE', '" + nom + "', '" + prenom + "', STR_TO_DATE('" + birthday + "', '%d/%m/%Y')" + "," + numeroEquipe +")";
             stmt.executeUpdate(query3);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-
-
-
-
 	public void fermerConnexion() {
         try {
             if (connection != null) {
