554be217783da729ae802273377471151c71cd9e
Yifan0105 <yifanshi0105@gmail.com>
2023-11-09 19:07:08 +0100

    Update MainMenu.java

--- src/riskGame/controller/MainMenu.java
+++ src/riskGame/controller/MainMenu.java
@@ -3,17 +3,6 @@ package riskGame.controller;
 import riskGame.RiskGame;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainMenu {
 
